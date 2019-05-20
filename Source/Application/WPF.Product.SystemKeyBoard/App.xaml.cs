9a51147877040eb18332102201d1f7fd31cd7152
Yan Qian <utemfjtn@163.com>
2019-05-20 16:41:46 +0800

    Fork it from HebianGu. Rename Project to WPF.

---
+++ Source/Application/WPF.Product.SystemKeyBoard/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using WPF.Product.UserControls.SystemKeyBoard;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF.Product.SystemKeyBoard
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+      
+    }
+}
